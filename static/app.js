f3733fcc0c0ef824daa60fe3abc26b06f75dc541
devagrahari9121 <dev.agrahari.9121@gmail.com>
2025-07-30 18:14:10 +0530

    i changed css and added a alert on apply now button team member devansh

---
+++ static/app.js
@@ -0,0 +1,19 @@
+let apply = document.querySelector(".btn1");
+let apply2 = document.querySelector(".btn2");
+let apply3 = document.querySelector(".btn3");
+let apply4 = document.querySelector(".btn4");
+
+apply.addEventListener('click', function(){
+    alert("Please login first");
+})
+
+apply2.addEventListener('click', function(){
+    alert("please login first");
+})
+apply3.addEventListener('click', function(){
+    alert("please login first");
+})
+
+apply4.addEventListener('click', function(){
+    alert("We wil notify!! when more jobs available");
+})
